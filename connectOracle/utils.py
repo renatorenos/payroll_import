d2dee3d6a4e9a68eef43c75e4e0f63aea8e84003
Renato Souza <renatorenos@gmail.com>
2024-04-15 16:02:57 -0300

    ADD: files for connecting to the Oracle database

---
+++ connectOracle/utils.py
@@ -0,0 +1,30 @@
+import oracledb
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def check_connection() -> str:
+    oracledb.init_oracle_client()
+    connection = oracledb.connect(  user=os.getenv("DB_USER"), 
+                                    password=os.getenv("DB_PASSWORD"), 
+                                    dsn=os.getenv("DB_DSN") )
+    cursor  = connection.cursor()
+
+    version = "Database version: " + connection.version
+    cursor.close()
+    return version
+
+def sql_query(query : str) -> list:
+    oracledb.init_oracle_client()
+    connection = oracledb.connect(  user=os.getenv("DB_USER"), 
+                                password=os.getenv("DB_PASSWORD"), 
+                                dsn=os.getenv("DB_DSN") )
+    cursor  = connection.cursor()
+
+    cursor.execute(query)
+    result = cursor.fetchall()
+
+    cursor.close()
+    connection.close
+    return result
