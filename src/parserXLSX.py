46c894f98c719ed370f4378464ae60c376f4ebf7
Renato Souza <renatorenos@gmail.com>
2024-04-15 19:07:43 -0300

    ADD: parserFile_list method that returns a list

--- src/parserXLSX.py
+++ src/parserXLSX.py
@@ -1,7 +1,7 @@
 from openpyxl import load_workbook
 from loguru import logger
 
-def parserFile(file):
+def parserFile_dict(file : str) -> dict:
     wb = load_workbook(filename= file, data_only=True)
     ws = wb['Planilha1']
 
@@ -19,6 +19,32 @@ def parserFile(file):
             raise Exception("Ocorreu um erro => Matricula em branco")
         value = str(ws[valueCol + str(i)].value).replace(" ", "").replace(".","").replace(",","")
         
+        logger.debug(f'[{i}] = {str(key).replace(" ", "")} : {value}')
+
         dic.update({ str(key).replace(" ", "") : value })
     
     return dic
+
+def parserFile_list(file : str) -> list:
+    wb = load_workbook(filename= file, data_only=True)
+    ws = wb['Planilha1']
+
+    matriculationCol = 'A'
+    valueCol = 'B'
+    logger.debug("Linhas da planilha[" + matriculationCol + "] = " + str(len(ws[matriculationCol])))   
+    logger.debug("Linhas da planilha[" + valueCol + "] = " + str(len(ws[matriculationCol])))
+
+    records = []
+    for i in range(1, len(ws[matriculationCol]) + 1):
+        key = ws[matriculationCol + str(i)].value
+        if key is None:
+            error = str(matriculationCol) + "[" + str(i) +"]" + " = Matricula em branco"
+            logger.error(error)
+            raise Exception("Ocorreu um erro => Matricula em branco")
+        value = str(ws[valueCol + str(i)].value).replace(" ", "").replace(".","").replace(",","")
+        k = str(key).replace(" ", "")
+
+        logger.debug(f'[{i}] = {k} : {value}')
+        records.append((k, value))
+    
+    return records
